43e07d200821558ee459b9cdb96234c8fb7e1aef
Merwann <merwannheret@gmail.com>
2025-10-10 15:05:19 +0200

    add : token

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 "use client";
 import Header from "./header";
+import { useState } from "react";
 
 export default function Home() {
   return (
@@ -11,8 +12,54 @@ export default function Home() {
 }
 
 function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("http://localhost:3001/login", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    let response = await res.json()
+    if (response["message"] == "Login successful") {
+      sessionStorage.setItem("token", response["token"])
+      const infoToken = await fetch("http://localhost:3001/token", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ token: response["token"] }),
+      });
+      let info = await infoToken.json()
+      if (info["message"] == "Token is valid and user is admin"){
+        window.location.href = "/volunteer_admin"
+      }else if (info["message"] == "Token is valid"){
+        window.location.href = "/volunteer"
+      }else{
+        alert("Problème avec le token")
+      }
+    } else {
+      alert("email ou mot de passe incorrect")
+    }
+  };
+
+  const isFormValid = Object.values(formData).every((val) => val.trim() !== "");
   return (
- <div className="bg-gray-50 flex justify-center py-10">
+    <div className="bg-gray-50 flex justify-center py-10">
       <div className="bg-white rounded-2xl shadow p-5 w-full max-w-sm">
         <h2 className="text-2xl font-bold mb-5 text-center">Login</h2>
         <form className="space-y-4">
@@ -21,10 +68,28 @@ function Login() {
             <input
               type="email"
               name="email"
+              onChange={handleChange}
               className="w-full border border-gray-300 rounded-lg p-2 focus:ring focus:ring-blue-200"
             />
           </div>
-          </form>
+          <div>
+            <label className="block font-medium">password</label>
+            <input
+              type="password"
+              name="password"
+              onChange={handleChange}
+              className="w-full border border-gray-300 rounded-lg p-2 focus:ring focus:ring-blue-200"
+            />
+          </div>
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            disabled={!isFormValid}
+            className="px-4 py-2 bg-[#039668] text-white rounded-lg w-full"
+          >
+            Envoyer
+          </button>
+        </form>
       </div>
     </div>
   );
--- src/app/volunteer_admin/page.js
+++ src/app/volunteer_admin/page.js
@@ -3,8 +3,37 @@
 import Header from "../header";
 import Navbar from "./navbar";
 import VolunteerList from "./volunteerList";
+import { useEffect , useState} from "react";
+
 
 export default function Home() {
+  const [isValidating, setIsValidating] = useState(true);
+  useEffect(() => {
+    async function token() {
+    let token = sessionStorage.getItem("token")
+    const infoToken = await fetch("http://localhost:3001/token", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ token: token }),
+    });
+    let info = await infoToken.json()
+    if (info["message"] !== "Token is valid and user is admin") {
+      window.location.href = "/"
+    }else{
+      setIsValidating(false);
+    }
+  }
+  token()
+  }, []);
+  if (isValidating) {
+    return (
+      <div className="flex items-center justify-center h-screen text-lg text-gray-700">
+        Vérification du token...
+      </div>
+    );
+  }
   return (
     <div>
       <Header />
@@ -13,6 +42,7 @@ export default function Home() {
     </div>
   );
 }
+
 
 
 
